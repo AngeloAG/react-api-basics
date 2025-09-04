0435ed5bc3a64fa21ad4ad5f766f95f02602976c
AngeloAG <angelo.ag.079@gmail.com>
2025-09-03 22:59:58 -0600

    Added simple api calls

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,29 @@
 import * as React from "react";
 import Layout from "./Layout";
+import axios from "axios";
 
 const App = () => {
-  return <Layout>Coming soon...</Layout>;
+	const [isLoading, setIsLoading] = React.useState(true);
+	React.useEffect(() => {
+		console.log("App mounted");
+		getData();
+	}, []);
+
+	const getData = () => {
+		const ENDPOINT =
+      "https://private-anon-8c2680aa4c-dailysmarty.apiary-mock.com/posts";
+		axios(ENDPOINT).then(response => {
+			setIsLoading(false);
+			return console.log("Data fetched:", response.data);
+		}).catch(error => {
+			setIsLoading(false);
+			return console.error("Error fetching data:", error);
+		});
+	};
+
+	const content = isLoading ? "Loading..." : "Data loaded";
+
+  return <Layout>{content}</Layout>;
 };
 
 export default App;
